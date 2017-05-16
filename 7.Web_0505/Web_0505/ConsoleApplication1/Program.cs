285a142852d74edd5166b621337381d697b9340f
yjcchen <yjcchen@YJCCHEN-PC>
2017-05-17 00:04:56 +0800

    web-html

---
+++ 7.Web_0505/Web_0505/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using JUIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var import = new JUIN.Service.ImportService();
+
+            var db = new JUIN.Repository.DataRepository();
+
+            //新增資料至資料庫
+            //var stations = import.FindStations(@"E:\water.xml");
+
+            //stations.ToList().ForEach(x =>
+            //{
+            //    db.Creat(x);
+            //});
+
+            //讀取資料表資料
+            var stations = db.FindAllStations();
+
+            showData(stations);
+
+            Console.ReadKey();
+        }
+
+        public static void showData(List<Station> stations)
+        {
+
+            Console.WriteLine(string.Format("共收到{0}筆資料", stations.Count));
+
+            stations.ForEach(x =>
+            {
+                Console.WriteLine(string.Format("ID: {0}, 時間: {1}, 高度: {2}", x.StationIdentifier, x.RecordTime, x.WaterLevel));
+            });
+        }
+
+        
+    }
+}
---
+++ 7.Web_0505/Web_0505/Web/Controllers/DemoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult IndexMessage()
+        {
+            var statioinRepository = new JUIN.Repository.DataRepository();
+
+            var stations = statioinRepository.FindAllStations();
+
+            var message = string.Format("共收到{0}筆資料</br>", stations.Count);
+
+            stations.ForEach(x => {
+                message += string.Format("ID: {0}, 時間: {1}, 高度: {2}</br>", x.StationIdentifier, x.RecordTime, x.WaterLevel);
+            });
+
+            return Content(message);
+        }
+
+        public ActionResult Index(string userName = "")
+        {
+            var stationRepository = new JUIN.Repository.DataRepository();
+
+            var stations = stationRepository.FindAllStations();
+            //var message = string.Format("共收到{0}筆監測站的資料<br/>", stations.Count);
+            //stations.ForEach(x =>
+            //{
+            //    message += string.Format("站點名稱：{0},地址:{1}<br/>", x.ObservatoryName, x.LocationAddress);
+
+
+            //});
+            ViewBag.UserName = userName;
+            ViewBag.Stations = stations;
+
+            return View(stations);
+        }
+    }
+}
