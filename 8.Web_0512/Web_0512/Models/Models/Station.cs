75574318d7f63865a85588992e3b8ed4c5884c9f
yjcchen <yjcchen@YJCCHEN-PC>
2017-05-18 23:28:49 +0800

    0512

---
+++ 8.Web_0512/Web_0512/Models/Models/Station.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUIN.Models
+{
+    public class Station
+    {
+        public string StationIdentifier { get; set; }
+        public string RecordTime { get; set; }
+        public string WaterLevel { get; set; }
+    }
+}
